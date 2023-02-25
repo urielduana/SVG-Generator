be34b106b18d48ade197237f18689cb115c57bee
Vadose <uri250602@outlook.es>
2023-02-25 02:30:59 -0600

    tkinter

--- SVGgenerador.py
+++ SVGgenerador.py
@@ -1,70 +1,74 @@
 import os
-from os import remove
 import re
+from os import remove
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF
+from tkinter import messagebox
 
-def dividir_strings(listaTotal, max_caracteres):
+def split_strings(data, max_chars):
     dividers = [" ", "-", ","]
-
-    for i, lista in enumerate(listaTotal):
-        for j, string in enumerate(lista):
-            if len(string) > max_caracteres:
+    for i, line in enumerate(data):
+        for j, string in enumerate(line):
+            if len(string) > max_chars:
                 idx = -1
                 for divider in dividers:
-                    temp_idx = string[:max_caracteres].rfind(divider)
+                    temp_idx = string[:max_chars].rfind(divider)
                     if temp_idx > idx:
                         idx = temp_idx
                 if idx == -1:
-                    idx = max_caracteres - 1
-                listaTotal[i][j:j + 1] = [string[:idx+1], string[idx+1:]]
-    
-    return listaTotal
+                    idx = max_chars - 1
+                data[i][j:j + 1] = [string[:idx+1], string[idx+1:]]
+    return data
 
-def generar_svg_pdf_por_archivo(input_file, numDeColumnas, directorio, directorioBases, max_caracteres):
-    # Crear directorio de salida
-    if not os.path.exists(directorio):
-        os.makedirs(directorio)
+def generate_svg_pdf_per_file(input_file, num_columns, output_dir, base_dir, max_chars):
+    # Create output directory
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    fi = open(input_file, "r", encoding='utf8')
-    real = []
+    with open(input_file, "r", encoding='utf8') as f:
+        real = [line.strip() for line in f if line.strip()]
 
-    for linea in fi:
-        if linea != '\n':
-            linea = re.sub("\\n", "", linea)
-            linea = re.sub("\\t", "", linea)
+    # First data extraction
+    data = [real[i:i + num_columns] for i in range(0, len(real), num_columns)]
 
-            real.append(linea)
+    # Split strings if they exceed max_chars characters
+    data = split_strings(data, max_chars)
 
-    # Primera extracción de datos
-    listaTotal = []
-    for i in range(0, len(real), numDeColumnas):
-        listaTotal.append(real[i:i + numDeColumnas])
+    # Generate PDF
+    generated_files = []
+    for i, line in enumerate(data):
+        with open(base_dir + f"Base_{len(line)}.svg", "r", encoding='utf8') as base_f, \
+             open(output_dir + line[0].split(":")[1] + ".svg", 'w', encoding='utf8') as write:
+            for base_line in base_f:
+                for j in range(len(line)):
+                    base_line = re.sub(f"CAMBIAR{j}AQUI", line[j], base_line)
+                write.write(base_line)
 
-    # Dividir strings si superan los max_caracteres caracteres
-    listaTotal = dividir_strings(listaTotal, max_caracteres)
+        drawing = svg2rlg(output_dir + line[0].split(":")[1] + ".svg")
+        renderPDF.drawToFile(drawing, output_dir + line[0].split(":")[1] + ".pdf")
+        remove(output_dir + line[0].split(":")[1] + ".svg")
+        generated_files.append(line[0].split(":")[1] + ".pdf")
 
-    #Generar PDF
-    for i, lista in enumerate(listaTotal):
-        read = open(directorioBases + "Base_" +str(len(lista))+".svg", "r",  encoding='utf8')
-        write= open(directorio + listaTotal[i][0].split(":")[1] +".svg", 'w', encoding='utf8')
-        for linea in read:
-            for j in range(len(lista)):
-                linea = re.sub("CAMBIAR"+str(j)+"AQUI",lista[j],linea)
-            write.write(linea)
-        write.close()
+    # Display generated files, grouping them by 30 if there are more than 30
+    num_files = len(generated_files)
+    if num_files <= 30:
+        messagebox.showinfo("PDF Generation", f"{num_files} PDFs were generated:\n" + "\n".join(generated_files))
+    else:
+        num_groups = num_files // 30 + (1 if num_files % 30 != 0 else 0)
+        for i in range(num_groups):
+            group_start = i * 30
+            group_end = (i + 1) * 30
+            group_files = generated_files[group_start:group_end]
+            messagebox.showinfo("PDF Generation", f"{len(group_files)} PDFs were generated (group {i+1}/{num_groups}):\n" + "\n".join(group_files))
 
-        drawing = svg2rlg(directorio + listaTotal[i][0].split(":")[1] +".svg")
-        renderPDF.drawToFile(drawing, directorio + listaTotal[i][0].split(":")[1] +".pdf")
-        remove(directorio + listaTotal[i][0].split(":")[1] +".svg")
 
-    fi.close()
 
-# Ejemplo de uso
+
+# Example usage
 input_file = "datos.txt"
-numDeColumnas = 10
-directorio = "C:/Users/Vadose/OneDrive/Documentos/TAGS/prueba/"
-directorioBases = "bases/"
-max_caracteres = 60
+num_columns = 10
+output_dir = "C:/Users/Vadose/OneDrive/Documentos/TAGS/prueba/"
+base_dir = "bases/"
+max_chars = 60
 
-generar_svg_pdf_por_archivo(input_file, numDeColumnas, directorio, directorioBases, max_caracteres)
+generate_svg_pdf_per_file(input_file, num_columns, output_dir, base_dir, max_chars)
