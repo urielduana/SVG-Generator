b35ad820c9c18ba6084d6f0a6f5a92f25f37e8e6
Vadose <uri250602@outlook.es>
2023-02-25 00:36:46 -0600

    Division de string de array de array

--- SVGgenerador.py
+++ SVGgenerador.py
@@ -1,83 +1,58 @@
 from os import remove
 import re
-
-
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF
 
-directorio = "C:/Users/Vadose/OneDrive/Documentos/Tags/Resultados/prueba/"
+directorio = "C:/Users/Vadose/OneDrive/Documentos/TAGS/1max_caracteres2/"
 numDeColumnas = 10
-fileData = open("datos.txt", "r",  encoding='utf8')
-fileTitle = open("titulo.txt", "r",  encoding='utf8')
-
+fi = open("datos.txt", "r", encoding='utf8')
+max_caracteres = 60
+real = []
 multiArray = []
 
 
-# Funcion para modificar el contenido
+# Función para modificar el contenido
 def modificarArray(x, y):
-    if(x == 0):
+    if x == 0:
         multiArray[x][y] = re.sub('', "", multiArray[x][y])
-    if(x == 2):
+    if x == 2:
         multiArray[x][y] = re.sub("", "", multiArray[x][y])
 
 
-def generarTitulo():
-    title = []
+for linea in fi:
+    print(linea)
+    if linea != '\n':
+        linea = re.sub("\\n", "", linea)
+        linea = re.sub("\\t", "", linea)
 
-    for linea in fileTitle:
-        if linea != '\n':
-            linea = re.sub("\\n", "", linea)
-            linea = re.sub("\\t", "", linea)
+        real.append(linea)
 
-            title.append(linea)
-    return title
+# Primera extracción de datos
+listaTotal = []
+for i in range(0, len(real), numDeColumnas):
+    listaTotal.append(real[i:i + numDeColumnas])
 
+# Dividir strings si superan los max_caracteres caracteres
+for i, lista in enumerate(listaTotal):
+    for j, string in enumerate(lista):
+        if len(string) > max_caracteres:
+            idx = string[:max_caracteres].rfind(" ")
+            if idx == -1:
+                idx = string[:max_caracteres].rfind("-")
+            if idx == -1:
+                idx = string[:max_caracteres].rfind(",")
+            if idx == -1:
+                idx = 59
+            listaTotal[i][j:j + 1] = [string[:idx+1], string[idx+1:]]
 
-def generarArray():
-    real = []
-
-    # Quitar saltos de linea
-    for linea in fileData:
-        if linea != '\n':
-            linea = re.sub("\\n", "", linea)
-            linea = re.sub("\\t", "", linea)
-
-            real.append(linea)
-
-    # Primera extraccion de datos
-    listaTotal = []
-    for i in range(0, len(real), numDeColumnas):
-        listaTotal.append(real[i:i+numDeColumnas])
-
-    return listaTotal
-
-
-titulo = generarTitulo()
-tag = generarArray()
-newArray = []
-
-for tags in tag:
-    miniArray = []
-    for linea, title in zip(tags, titulo):
-        a = title + ": " + linea
-        if len(a) > 66:
-            print(a)
-        miniArray.append(a)
-    newArray.append(miniArray)
-
-
-# #Acomodo de datos en Multi array
-# for f in range(numDeColumnas):
-#     multiArray.append([])
-#     for i in range(len(listaTotal)):
-#         multiArray[f].append(listaTotal[i][f])
-
-
-# for y in range(len(multiArray[0])):
-#     for x in range(len(multiArray)):
-#         #MODIFICACION DE ARRAY
-#         modificarArray(x, y)
-
+# Imprimir resultados
+print("Longitud de listaTotal:", len(listaTotal))
+for i, lista in enumerate(listaTotal):
+    print("-----------------")
+    print("Array", i+1, "- longitud:", len(lista))
+    print("-----------------")
+    for j, string in enumerate(lista):
+        print("Elemento", j+1, "-", string)
 
 # y=0
 
@@ -94,9 +69,6 @@ for tags in tag:
 #     y=y+1
 #     write.close()
 
-#     drawing = svg2rlg(directorio+"/" +f+".svg")
-#     renderPDF.drawToFile(drawing, directorio +"/"+ str(f) +".pdf")
+#     drawing = svg2rlg(directorio + str(f) +".svg")
+#     renderPDF.drawToFile(drawing, directorio + str(f) +".pdf")
 #     remove(directorio + str(f) +".svg")
-
-# print(real)
-# print(multiArray)
